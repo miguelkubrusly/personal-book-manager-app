803e6578b0e8f03ea24f2dad9d8c1f23bcb3eb5f
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-25 20:16:45 -0300

    feat: add App first draw and create BookCreate
    -BookCreate receives a function as a prop, and use it to create book
    -The function itself is not properly defined in App yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import BookCreate from "./components/BookCreate";
+
+// export interface Book {
+//   id: number;
+//   title: string;
+// }
 
 function App() {
-  return <div>App Here</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title: string) => {
+    console.log("creating book with title", title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookCreate.tsx
+++ src/components/BookCreate.tsx
@@ -1,5 +1,47 @@
-function BookCreate() {
-  return <div>BookCreate App Here</div>;
+import { useState, ChangeEvent, FormEvent } from "react";
+
+export interface BookCreateProps {
+  onCreate: (title: string) => void;
+}
+
+function BookCreate({ onCreate }: BookCreateProps) {
+  const [title, setTitle] = useState("");
+
+  const formatTitle = (title: string) => {
+    let formattedTitle: string = "";
+
+    const titleArr = title.trim().split(" ");
+    if (titleArr.length === 0) {
+      return "";
+    }
+    const capitalizedArr = titleArr.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    capitalizedArr.length === 1
+      ? (formattedTitle = capitalizedArr[0])
+      : (formattedTitle = capitalizedArr.join(" "));
+
+    return formattedTitle;
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onCreate(formatTitle(title)!);
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form className="book-create" onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input onChange={handleChange} value={title} />
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
