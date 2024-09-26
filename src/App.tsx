425d7a3c4ecd0aeeea40ea8c070b6968d4322d1c
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-26 00:28:11 -0300

    feat: use crypo-random-string to generate UUID
    -UUID stands for Universally Unique Identifier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import crypto from "crypto-random-string";
 
 export interface Book {
-  id: number;
+  id: string;
   title: string;
 }
 
@@ -10,8 +11,10 @@ function App() {
   const [books, setBooks] = useState<Book[]>([]);
 
   const createBook = (title: string) => {
-    const updatedBooks: Book[] = [...books, { id: books.length + 1, title }];
+    const uuid: string = crypto(16);
+    const updatedBooks: Book[] = [...books, { id: uuid, title }];
     setBooks(updatedBooks);
+    console.log(updatedBooks);
   };
 
   return (
