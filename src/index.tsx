fbddf4c8bb6ae825e565c8a2120be660994e38b6
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-25 23:32:26 -0300

    style: add css styling
    -this css was provided by the Udemy course

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM, { Root } from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 const el: HTMLElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(el) as Root;
