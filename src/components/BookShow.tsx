1a31434378d8722a1e41aafa4a0e2bad748e55e1
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-27 11:48:29 -0300

    feat: add delete logic on delete button click

--- src/components/BookShow.tsx
+++ src/components/BookShow.tsx
@@ -6,7 +6,20 @@ interface BookShowProps {
 }
 
 function BookShow({ book, onDelete }: BookShowProps) {
-  return <div className="book-show">{book.title}</div>;
+  const handleClick = () => {
+    onDelete(book.id);
+  };
+
+  return (
+    <div className="book-show">
+      {book.title}
+      <div className="actions">
+        <button className="delete" onClick={handleClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default BookShow;
