3a6cade26fd71d1a62810226fa7207cc4d9fa4e1
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-27 11:18:28 -0300

    feat: create BookShow
    -for now, it only shows book title

--- src/components/BookShow.tsx
+++ src/components/BookShow.tsx
@@ -1,5 +1,12 @@
-function BookShow() {
-  return <div>BookShow App Here</div>;
+import { Book } from "../App";
+
+interface BookShowProps {
+  book: Book;
+  onDelete: (id: string) => void;
+}
+
+function BookShow({ book, onDelete }: BookShowProps) {
+  return <div className="book-show">{book.title}</div>;
 }
 
 export default BookShow;
