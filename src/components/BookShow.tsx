4c6d879db6aa772cb1b67ecc417f76a93f3a7a24
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-27 15:08:55 -0300

    feat: add random image to book + title to app

--- src/components/BookShow.tsx
+++ src/components/BookShow.tsx
@@ -32,6 +32,10 @@ function BookShow({ book, onDelete, onEdit }: BookShowProps) {
 
   return (
     <div className="book-show">
+      <img
+        src={`https://picsum.photos/seed/${book.id}/300/200`}
+        alt={book.title}
+      />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={editClick}>
