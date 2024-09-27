9f387fdc6afd72c464107a4d4ecf0906c23fb873
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-27 13:32:25 -0300

    feat: create BookEdit component
    -a function to return edit component in BookShow
    -it tracks changes on title
    -it auto focus and auto select title at start

--- src/components/BookEdit.tsx
+++ src/components/BookEdit.tsx
@@ -1,5 +1,35 @@
-function BookEdit() {
-  return <div>BookEdit App Here</div>;
+import { ChangeEvent, FormEvent, FocusEvent, useState } from "react";
+import { Book } from "../App";
+
+function BookEdit({ book }: { book: Book }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("new title:", title);
+  };
+
+  const autoSelect = (event: FocusEvent<HTMLInputElement>) => {
+    (event.target as HTMLInputElement).select();
+  };
+
+  return (
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label htmlFor="book-title">Title</label>
+      <input
+        className="input"
+        value={title}
+        onChange={handleChange}
+        autoFocus
+        onFocus={autoSelect}
+      />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
 
 export default BookEdit;
