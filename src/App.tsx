4d65a29e1b732f2342dcb12a98807f09ede458d1
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-27 10:48:34 -0300

    feat: add deleteBookById(id) in App
    -gonna use this function inside book show

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 export interface Book {
   id: string;
@@ -10,14 +11,22 @@ function App() {
   const [books, setBooks] = useState<Book[]>([]);
 
   const createBook = (title: string) => {
-    const uuid = crypto.randomUUID().replace(/-/g, "").slice(0, 8);
+    const uuid: string = crypto.randomUUID().replace(/-/g, "").slice(0, 8);
     const updatedBooks: Book[] = [...books, { id: uuid, title }];
     setBooks(updatedBooks);
     console.log(updatedBooks);
   };
 
+  const deleteBookById = (id: string) => {
+    const updatedBooks: Book[] = books.filter((book: Book) => {
+      book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
