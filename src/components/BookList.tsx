362c545e408a2dec1e31d4c081f5e0cc4c1cdeea
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-27 11:04:45 -0300

    feat: pass onDelete from App to B.Show

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,5 +1,20 @@
-function BookList() {
-  return <div>BookList App Here</div>;
+import BookShow from "./BookShow";
+import { Book } from "../App";
+
+function BookList({
+  books,
+  onDelete,
+}: {
+  books: Book[];
+  onDelete: (id: string) => void;
+}) {
+  const renderedBooks = books.map((book: Book) => (
+    <div>
+      <BookShow key={book.id} book={book} onDelete={onDelete} />
+    </div>
+  ));
+
+  return <div>{renderedBooks}</div>;
 }
 
 export default BookList;
